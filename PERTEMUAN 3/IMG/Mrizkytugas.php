45747823fbeec2e9f781d4ebbae4feb56b474bb8
rizkyansyah-12 <rizkyansyah920@gmail.com>
2020-09-17 10:30:31 +0700

    Materi Pertemuan 1-5

---
+++ PERTEMUAN 3/IMG/Mrizkytugas.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
+<!-- saved from url=(0051)http://localhost/iking/pertemuan%201/pertemuan%203/ -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <title>Index of /iking/pertemuan 1/pertemuan 3</title>
+ </head>
+ <body>
+<h1>Index of /iking/pertemuan 1/pertemuan 3</h1>
+  <table>
+   <tbody><tr><th valign="top"><img src="./Mrizkytugas_files/blank.gif" alt="[ICO]"></th><th><a href="http://localhost/iking/pertemuan%201/pertemuan%203/?C=N;O=D">Name</a></th><th><a href="http://localhost/iking/pertemuan%201/pertemuan%203/?C=M;O=A">Last modified</a></th><th><a href="http://localhost/iking/pertemuan%201/pertemuan%203/?C=S;O=A">Size</a></th><th><a href="http://localhost/iking/pertemuan%201/pertemuan%203/?C=D;O=A">Description</a></th></tr>
+   <tr><th colspan="5"><hr></th></tr>
+<tr><td valign="top"><img src="./Mrizkytugas_files/back.gif" alt="[PARENTDIR]"></td><td><a href="http://localhost/iking/pertemuan%201/">Parent Directory</a>       </td><td>&nbsp;</td><td align="right">  - </td><td>&nbsp;</td></tr>
+<tr><td valign="top"><img src="./Mrizkytugas_files/text.gif" alt="[TXT]"></td><td><a href="http://localhost/iking/pertemuan%201/pertemuan%203/TUGAS%20M.RIZKYANSYAH.php">TUGAS M.RIZKYANSYAH.php</a></td><td align="right">2020-09-03 17:05  </td><td align="right">502 </td><td>&nbsp;</td></tr>
+<tr><td valign="top"><img src="./Mrizkytugas_files/unknown.gif" alt="[   ]"></td><td><a href="http://localhost/iking/pertemuan%201/pertemuan%203/latihan">latihan</a>                </td><td align="right">2020-08-27 07:59  </td><td align="right">  0 </td><td>&nbsp;</td></tr>
+<tr><td valign="top"><img src="./Mrizkytugas_files/text.gif" alt="[TXT]"></td><td><a href="http://localhost/iking/pertemuan%201/pertemuan%203/latihan1.php">latihan1.php</a>           </td><td align="right">2020-09-03 16:07  </td><td align="right">567 </td><td>&nbsp;</td></tr>
+<tr><td valign="top"><img src="./Mrizkytugas_files/text.gif" alt="[TXT]"></td><td><a href="http://localhost/iking/pertemuan%201/pertemuan%203/latihan%202.php">latihan 2.php</a>          </td><td align="right">2020-09-02 14:52  </td><td align="right">526 </td><td>&nbsp;</td></tr>
+<tr><td valign="top"><img src="./Mrizkytugas_files/text.gif" alt="[TXT]"></td><td><a href="http://localhost/iking/pertemuan%201/pertemuan%203/tugas3.php">tugas3.php</a>             </td><td align="right">2020-09-03 16:07  </td><td align="right">113 </td><td>&nbsp;</td></tr>
+   <tr><th colspan="5"><hr></th></tr>
+</tbody></table>
+<address>Apache/2.4.43 (Win64) OpenSSL/1.1.1g PHP/7.2.31 Server at localhost Port 80</address>
+
+</body></html>
